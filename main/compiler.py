dac77865fcb41d8ab64c898c824c05d44c9506c2
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-09-26 19:26:31 +1000

    Update compiler.py

--- main/compiler.py
+++ main/compiler.py
@@ -1,8 +1,8 @@
-from transpile_operations import *
+
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
 from tkinter import messagebox as tkmsg
-from tkinter import filedialog
+from tkinter import filedialog; import time
 window = Tk()
 
 window.title("Python-Lua Compiler")
@@ -10,7 +10,8 @@ window.configure(bg="#000000")
 window.tk.call('tk', 'scaling', 3.0) # Fix resolution
 
 # Func
-
+def msgbox(title, desc):
+  tkmsg.showinfo(title, desc)
 def openDialog():
   openableTypes = [("All Files", "*"), ("Python Files", "*.py"), ("Text Files", "*.txt")]
   openedPath = filedialog.askopenfilename(filetypes=openableTypes)
@@ -21,19 +22,22 @@ def openDialog():
   text_box.insert(1.0, readOpened)
   msgbox("Opened", "File opened successfully.")
 
-def pyluaTranspile():
-  toParse = text_box.get(1.0, END)
-  text_box.delete(1.0, END)
-  text_box.insert(1.0, '-- Result:\n' + "\n".join(result_array))
 
 # widgets
 text_box = ScrolledText(background="#000000", foreground="#FFFFFF")
-
+# Operations
+class to:
+    result = []
+def operations():
+    openOperations = open("transpile_operations.py", "r")
+    exec(openOperations.read()); openOperations.close()
 # menus
 _menu = Menu()
 mainMenu = Menu(_menu, tearoff=0)
 mainMenu.add_command(label="Open", command=openDialog)
 mainMenu.add_command(label="Help", command= lambda: tkmsg.showinfo("Help", "Check repository for details."))
-mainMemu.add_command(label="Compile", command=pyluaTranspile)
-
+mainMenu.add_command(label="Compile", command=operations)
 _menu.add_cascade(label="Menu", menu=mainMenu)
+window.config(menu=_menu)
+text_box.pack()
+window.mainloop()
