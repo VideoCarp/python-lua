5c51465edb1bc53eea8305433cf77a5a97f0273d
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-09-26 14:38:11 +1000

    Update compiler.py

--- main/compiler.py
+++ main/compiler.py
@@ -1,1 +1,7 @@
 from transpile_operations import *
+from tkinter import *
+window = Tk()
+
+window.title("Python-Lua Compiler")
+window.configure(bg="#000000")
+window.tk.call('tk', 'scaling', 3.0) # Fix resolution
