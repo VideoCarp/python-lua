ff3b850c4b6005a823885484faf5a8434c9280cf
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-09-26 14:39:34 +1000

    Update compiler.py

--- main/compiler.py
+++ main/compiler.py
@@ -5,3 +5,15 @@ window = Tk()
 window.title("Python-Lua Compiler")
 window.configure(bg="#000000")
 window.tk.call('tk', 'scaling', 3.0) # Fix resolution
+
+# Func
+
+def openDialog():
+  openableTypes = [("All Files", "*"), ("Python Files", "*.py"), ("Text Files", "*.txt")]
+  openedPath = filedialog.askopenfilename(filetypes=openableTypes)
+  opened = open(openedPath)
+  readOpened = opened.read()
+  opened.close()
+  text_box.delete(1.0, END)
+  text_box.insert(1.0, readOpened)
+  msgbox("Opened", "File opened successfully.")
