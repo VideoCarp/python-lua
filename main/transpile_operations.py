48c21811303bdc472f242c72669c5a2ede2ad1e2
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-10-02 18:11:48 +1000

    Update transpile_operations.py

--- main/transpile_operations.py
+++ main/transpile_operations.py
@@ -62,8 +62,10 @@ for line in split_array:
               result_array.append(line.replace("max(", "math.max("))
         elif "min(" in line and ")" in line:
               result_array.append(line.replace("min(", "math.min("))
-        elif "lamba" in line and ":" in line:
-              result_array.append(line.replace("lamda", "function").replace(" :", "()").replace(":", "("))
+        elif "lamba" in line and ":" in line and " " in line:
+              result_array.append(line.replace("lamda", "function").replace(" ", "(").replace(":", ")"))
+        elif "lambda" in line and ":" in line and " " not in line:
+              result_array.append(line.replace("lambda", "function").replace(":", "()"))
         else:
             result_array.append(line)
         text_box.delete(1.0, END)
