ed3128903aeff073f77cfb4ed217ee795e33e1be
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-10-03 15:29:16 +1000

    Update transpile_operations.py

--- main/transpile_operations.py
+++ main/transpile_operations.py
@@ -68,7 +68,7 @@ for line in split_array:
         elif "lambda" in line and ":" in line and " " not in line:
               result_array.append(line.replace("lambda", "function").replace(":", "()"))
         elif "for " in line and ":" in line:
-              result_array.append(line.replace(":", "do"))
+              result_array.append(line.replace(":", " do"))
         else:
             result_array.append(line)
         text_box.delete(1.0, END)
